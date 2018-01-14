259892735a8288eb74188ce6277130f537b66d35
hamedvama <hamedbaba8@gmail.com>
2018-01-14 12:35:12 +0100

    first commit

---
+++ src/main/java/fr/istic/master1/sir/tpmaven/App.java
@@ -0,0 +1,27 @@
+package fr.istic.master1.sir.tpmaven;
+
+/**
+ * Hello world world!
+ *
+ */
+public class App 
+{
+	
+	public String alpha;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void afficher() {
+    	if(alpha == "beta") {
+    		
+    	}else if(alpha == "gama") {
+    		
+    	}else if(alpha == "lamda") {
+    		
+    	}else {
+    		
+    	}
+    }
+}
